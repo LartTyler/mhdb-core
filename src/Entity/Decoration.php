a65e548c44709fc20d032c559529e08d7b02e897
Tyler Lartonoix <tyler@lartonoix.com>
2025-01-17 11:55:35 -0500

    Add decoration endpoints

---
+++ src/Entity/Decoration.php
@@ -0,0 +1,89 @@
+<?php
+	namespace App\Entity;
+
+	use App\Api\AsCrudEntity;
+	use App\Api\Models\DecorationModel;
+	use App\Api\Transformers\DecorationTransformer;
+	use DaybreakStudios\Utility\DoctrineEntities\EntityInterface;
+	use Doctrine\Common\Collections\ArrayCollection;
+	use Doctrine\Common\Collections\Collection;
+	use Doctrine\Common\Collections\Selectable;
+	use Doctrine\ORM\Mapping as ORM;
+
+	#[ORM\Entity]
+	#[ORM\Table(name: 'decorations')]
+	#[AsCrudEntity(
+		basePath: '/decorations',
+		transformer: DecorationTransformer::class,
+		dtoClass: DecorationModel::class,
+		strict: [
+			'skills' => [
+				'skill' => [
+					'*',
+					'-id',
+					'-name',
+				],
+			],
+		],
+	)]
+	class Decoration implements EntityInterface {
+		use EntityTrait;
+
+		#[ORM\Column(nullable: true)]
+		private ?string $name;
+
+		#[ORM\Column(options: ['unsigned' => true])]
+		private int $slot;
+
+		#[ORM\Column(options: ['unsigned' => true])]
+		private int $rarity;
+
+		/**
+		 * @var Selectable<SkillRank>&Collection<SkillRank>
+		 */
+		#[ORM\ManyToMany(targetEntity: SkillRank::class)]
+		#[ORM\JoinTable(name: 'decoration_skills')]
+		private Collection&Selectable $skills;
+
+		public function __construct(string $name, int $slot, int $rarity) {
+			$this->name = $name;
+			$this->slot = $slot;
+			$this->rarity = $rarity;
+
+			$this->skills = new ArrayCollection();
+		}
+
+		public function getName(): ?string {
+			return $this->name;
+		}
+
+		public function setName(?string $name): static {
+			$this->name = $name;
+			return $this;
+		}
+
+		public function getSlot(): int {
+			return $this->slot;
+		}
+
+		public function setSlot(int $slot): static {
+			$this->slot = $slot;
+			return $this;
+		}
+
+		public function getRarity(): int {
+			return $this->rarity;
+		}
+
+		public function setRarity(int $rarity): static {
+			$this->rarity = $rarity;
+			return $this;
+		}
+
+		/**
+		 * @return Collection<SkillRank>&Selectable<SkillRank>
+		 */
+		public function getSkills(): Selectable&Collection {
+			return $this->skills;
+		}
+	}
